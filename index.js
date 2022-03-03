74da36406bba8237abf47899b5e8efb10ae26971
Miroslav Kovář <mira.kovar@gmail.com>
2022-03-03 12:44:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-function getCountryInfo(callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        callback(JSON.parse(this.responseText));
-    };
-    xhr.open("GET", "http://ip-api.com/json");
-    xhr.send();
-}
+(function () {
+    function getCountryInfo(callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            callback(JSON.parse(this.responseText));
+        };
+        xhr.open("GET", "http://ip-api.com/json");
+        xhr.send();
+    }
 
-function setCookie(cName, cValue, expDays) {
-    let date = new Date();
-    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
-}
+    function setCookie(name, value, expiration) {
+        var date = new Date();
+        date.setTime(date.getTime() + (expiration * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + "; " + expires + "; path=/";
+    }
 
-function getCookie(cName) {
-    const name = cName + "=";
-    const cDecoded = decodeURIComponent(document.cookie);
-    const cArr = cDecoded.split('; ');
-    let res;
-    cArr.forEach(val => {
-        if (val.indexOf(name) === 0) res = val.substring(name.length);
-    })
-    return res;
-}
+    function getCookie(name) {
+        name = name + "=";
+        var response;
 
-if (!getCookie('stop_war')) {
-    getCountryInfo(function (response) {
-        setCookie('stop_war', 'now!', 1);
-        if (response.countryCode === 'RU') {
-            window.location = 'https://web4ukraine.org/stop?url=' + window.location.href;
-        }
-    });
-}
+        decodeURIComponent(document.cookie)
+            .split('; ')
+            .forEach(function (val) {
+                if (val.indexOf(name) === 0) {
+                    response = val.substring(name.length);
+                }
+            });
+
+        return response;
+    }
+
+    if (!getCookie('stop_war')) {
+        getCountryInfo(function (response) {
+            setCookie('stop_war', 'now!', 1);
+            if (response.countryCode === 'RU') {
+                window.location = 'https://web4ukraine.org/stop?url=' + window.location.href;
+            }
+        });
+    }
+}());
